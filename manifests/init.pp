3f7a7bfccd75cbd73ed99f52797c2462cd1a7ba8
Slm0n87 <github@firebo.de>
2015-07-28 19:49:32 +0200

    Add default phpmyadmin-config from dpkg

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,12 +3,14 @@ class sist_phpmyadmin inherits sist_vars {
   File {
     owner   => 'root',
     group   => 'root',
+    require => Package['phpmyadmin'],
   }
 
   file { "/var/local/preseed/phpmyadmin.preseed":
     content => template('sist_phpmyadmin/phpmyadmin.preseed.erb'),
-    mode   => 600,
-    backup => false,
+    mode    => 600,
+    backup  => false,
+    require => undef,
   }->
   package { 'phpmyadmin':
     ensure => $ensure,
@@ -17,14 +19,13 @@ class sist_phpmyadmin inherits sist_vars {
 
   file { '/etc/phpmyadmin/apache.conf':
     source => 'puppet:///modules/sist_phpmyadmin/apache.conf',
-    require => Package['phpmyadmin'],
     notify  => Service['apache2'],
   }
   file { '/etc/phpmyadmin/htpasswd.setup':
     source => 'puppet:///modules/sist_phpmyadmin/htpasswd.setup',
-    require => Package['phpmyadmin'],
   }
-
-  #TODO: phpmyadmin-config (setup!)
+  file { '/etc/phpmyadmin/config.inc.php':
+    source => 'puppet:///modules/sist_phpmyadmin/config.inc.php',
+  }
 
 }
